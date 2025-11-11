f8320544816a7f8ca6e804865a3b1d5433de6c79
LiamMurphy53 <murli01quebo@gmail.com>
2025-11-10 17:56:49 -0700

    quadrotor feedback

--- QuadrotorEOMwithFeedback.m
+++ QuadrotorEOMwithFeedback.m
@@ -1,8 +1,9 @@
 function var_dot = QuadrotorEOMwithRateFeedback(t, var, g, m, I, nu, mu)
 
+    [Fc, Gc] = RotationDerivativeFeedback(var, m, g);   
 
+    d = 0.06;
+    motor_forces = ComputeMotorForces(Fc, Gc, d, km);   
 
-
-
-
+    var_dot = QuadrotorEOM(t, var, g, m, I, d, km, nu, mu, motor_forces);
 end
